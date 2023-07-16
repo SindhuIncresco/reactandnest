b015e1d336db0bb8b2b6b34b94348f9cb0a293d6
sindhuincresco <137244356+SindhuIncresco@users.noreply.github.com>
2023-07-16 09:18:47 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,48 @@
+// import { NestFactory } from '@nestjs/core';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { AppModule } from './app.module';
+// import * as Sentry from '@sentry/node';
+// import { error } from 'console';
+// require('dotenv').config();
+
+// async function bootstrap() {
+
+
+//   Sentry.init({
+//     dsn: process.env.SENTRY_DNS
+//   });
+
+//   try{
+ 
+//     // throw new Error("Sentry error check123")
+//     const app = await NestFactory.create(AppModule);
+//     const options = new DocumentBuilder()
+//       .setTitle('Job Application API')
+//       .setDescription('API for managing job applications')
+//       .setVersion('1.0')
+//       .build();
+//     const document = SwaggerModule.createDocument(app, options);
+//     SwaggerModule.setup('api', app, document);
+//     await app.listen(3000);
+//   }catch(error){
+//   Sentry.captureException(error)
+//   }
+// }
+// bootstrap();
+
+
+
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as Sentry from '@sentry/node';
-import { error } from 'console';
-require('dotenv').config();
 
 async function bootstrap() {
-
-
   Sentry.init({
-    dsn: process.env.SENTRY_DNS
+    dsn: process.env.SENTRY_DSN
   });
 
-  try{
- 
-    // throw new Error("Sentry error check123")
+  try {
     const app = await NestFactory.create(AppModule);
     const options = new DocumentBuilder()
       .setTitle('Job Application API')
@@ -24,13 +52,9 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api', app, document);
     await app.listen(3000);
-  }catch(error){
-  Sentry.captureException(error)
+  } catch (err) {
+    Sentry.captureException(err);
   }
-  
-  
 }
+
 bootstrap();
-
-
-
